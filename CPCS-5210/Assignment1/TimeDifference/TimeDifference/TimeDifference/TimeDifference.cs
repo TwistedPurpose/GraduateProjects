02b45839c9a65d51b64a77c398aa8b09c31bf1de
TwistedPurpose <sjludwig7@gmail.com>
2016-01-18 19:43:06 -0800

    Reorganization and Assignment 1 for CPCS 5610
    Reorganization of folder structure and checkin of near final product of
    assignment 1 for CPCS 5610

---
+++ CPCS-5210/Assignment1/TimeDifference/TimeDifference/TimeDifference/TimeDifference.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeDifference
+{
+    public class TimeDiff
+    {
+        public struct TimeOfDay
+        {
+            public int hour;
+            public int minute;
+            public bool isPM;
+
+            public TimeOfDay(int h, int m, bool p) { hour = h; minute = m; isPM = p; }
+        }
+
+        public struct TimeInterval
+        {
+            public int hours;
+            public int minutes;
+
+            public TimeInterval(int h, int m) { hours = h; minutes = m; }
+        }
+
+        private static readonly TimeInterval errorInterval;
+
+        static TimeDiff()
+        {
+            errorInterval = new TimeInterval(-1, -1);
+        }
+
+        public static TimeInterval computeTimeDiff(TimeOfDay early, TimeOfDay late)
+        {
+            TimeInterval result;
+
+            if (early.hour < 1 || early.hour > 12)
+            {
+                Console.WriteLine("Invalid early hour");
+                return errorInterval;
+            }
+            if (late.hour < 1 || late.hour > 12)
+            {
+                Console.WriteLine("Invalid late hour");
+                return errorInterval;
+            }
+            if (early.minute < 0 || early.minute > 60)
+            {
+                Console.WriteLine("Invalid early minute");
+                return errorInterval;
+            }
+            if (late.minute < 0 || late.minute > 60)
+            {
+                Console.WriteLine("Invalid early minute");
+                return errorInterval;
+            }
+
+            if (early.isPM != late.isPM)
+            {
+                result.hours = late.hour + 12 - early.hour;
+            }
+            else if (early.hour > late.hour)
+            {
+                result.hours = late.hour + 24 - early.hour;
+            }
+            else
+            {
+                result.hours = late.hour - early.hour;
+            }
+
+            if (early.minute == late.minute)
+            {
+                result.minutes = 0;
+            }
+            else if (early.minute > late.minute)
+            {
+                result.minutes = late.minute + 60 - early.minute;
+                result.hours -= 1;
+            }
+            else
+            {
+                result.minutes = late.minute - early.minute;
+            }
+
+            return result;
+        }
+
+        private static TimeOfDay? getTimeOfDayFromInput(string prompt)
+        {
+            try
+            {
+                Console.Write(prompt);
+                string userStr = Console.ReadLine();
+                var parts = userStr.Split(':');
+                var rest = parts[1].Split(' ');
+                int h = Convert.ToInt32(parts[0]);
+                int m = Convert.ToInt32(rest[0]);
+                bool p = false;
+                if (rest[1] == "PM" || rest[1] == "pm")
+                {
+                    p = true;
+                }
+                else if (rest[1] == "AM" || rest[1] == "am")
+                {
+                    p = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                    return null;
+                }
+                return new TimeOfDay(h, m, p);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid input");
+                return null;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TimeOfDay? early = null;
+            while (early.Equals(null))
+            {
+                early = getTimeOfDayFromInput("Enter early time: ");
+            }
+            TimeOfDay earlyTime = (TimeOfDay)early;
+
+            TimeOfDay? late = null;
+            while (late.Equals(null))
+            {
+                late = getTimeOfDayFromInput("Enter late time: ");
+            }
+            TimeOfDay lateTime = (TimeOfDay)late;
+
+            TimeInterval result = computeTimeDiff(earlyTime, lateTime);
+
+            if (result.Equals(errorInterval))
+            {
+                Console.WriteLine("computeTimeDiff reported an error");
+            }
+            else
+            {
+                Console.WriteLine("The difference is {0} hours and {1} minutes", result.hours, result.minutes);
+            }
+
+            // Require the user to press a key to exit window
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CPCS-5210/Assignment1/TimeDifference/TimeDifference/TimeDifferenceUnitTests/TimeDiffTests.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeDifference;
+
+namespace TimeDifferenceUnitTests
+{
+    [TestClass]
+    public class TimeDiffTests
+    {
+        public readonly int ERROR_INTERVAL = -1;
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_MorningAndEvening()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(3, 45, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(6, 10, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(14, 25);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours are incorrect");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes are incorrect");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_NoDifference()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(3, 45, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(3, 45, false);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(0, 0);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours are incorrect");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes are incorrect");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_12HourDifference()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(2, 0, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(2, 0, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(12, 0);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours are incorrect");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes are incorrect");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_MidnightBoundaryNightToMorning()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(11, 59, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 0, false);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(0, 1);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours are incorrect");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes are incorrect");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_MidnightBoundaryMorningToNight()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 0, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(11, 59, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(23, 59);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours are incorrect");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes are incorrect");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_MorningBoundaryMorningToAfternoon()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(11, 59, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 0, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(0, 1);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours are incorrect");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes are incorrect");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_MorningBoundaryAfternoonToMorning()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 0, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(11,59, false);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(23, 59);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours are incorrect");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes are incorrect");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_HourBoundaryOneMinute()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(1, 59, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(2, 0, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(0, 1);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours are incorrect");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes are incorrect");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_HourBoundaryWholeDay()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(2, 0, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(1, 59, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(23, 59);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours are incorrect");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes are incorrect");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidPositiveEarlyHourPM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(13, 0, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 0, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidPositiveLateHourPM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 0, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(13, 0, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidPositiveEarlyHourAM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(13, 0, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 0, false);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidPositiveLateHourAM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 0, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(13, 0, false);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidPositiveEarlyMinutePM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 60, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 0, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidPositiveLateMinutePM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 0, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 60, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidPositiveEarlyMinuteAM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 60, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 0, false);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidPositiveLateMinuteAM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 0, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 60, false);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidNegativeEarlyHourPM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(-1, 0, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 0, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidNegativeLateHourPM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 0, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(-1, 0, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidNegativeEarlyHourAM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(-1, 0, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 0, false);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidNegativeLateHourAM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 0, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(-1, 0, false);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidNegativeEarlyMinutePM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, -1, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 0, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidNegativeLateMinutePM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 0, true);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, -1, true);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidNegativeEarlyMinuteAM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, -1, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, 0, false);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+
+        [TestMethod]
+        public void TimeDifference_TimeDiff_InvalidNegativeLateMinuteAM()
+        {
+            TimeDiff.TimeOfDay early = new TimeDiff.TimeOfDay(12, 0, false);
+            TimeDiff.TimeOfDay late = new TimeDiff.TimeOfDay(12, -1, false);
+            TimeDiff.TimeInterval expected = new TimeDiff.TimeInterval(ERROR_INTERVAL, ERROR_INTERVAL);
+            TimeDiff.TimeInterval actual = TimeDiff.computeTimeDiff(early, late);
+            Assert.AreEqual(expected.hours, actual.hours, "Hours don't have validation");
+            Assert.AreEqual(expected.minutes, actual.minutes, "Minutes don't have validation");
+        }
+    }
+}
